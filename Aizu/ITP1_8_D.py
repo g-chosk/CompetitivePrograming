40caf51ba9604d39408ac3bc5bfa8fe2403aef0c
HIRATA <hirasato3104@gmail.com>
2021-12-26 05:45:54 +0900

    (p_-)

--- Aizu/ITP1_8_D.py
+++ Aizu/ITP1_8_D.py
@@ -1,2 +1,12 @@
 s   = input()
 p   = input()
+result  = 'No'
+for i in range(len(s)):
+    if i + len(p) > len(s):
+        if (s[i:] + s[:len(p) - len(s) + i]) == p:
+            result  = 'Yes'
+    else:
+        if s[i:i+len(p)] == p:
+            result  = 'Yes'
+
+print(result)
