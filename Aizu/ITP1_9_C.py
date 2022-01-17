db1a507e4bf0b6dba7f08049a17ba17d95e6f586
HIRATA <hirasato3104@gmail.com>
2022-01-18 03:01:26 +0900

    d(￣ ￣)

--- Aizu/ITP1_9_C.py
+++ Aizu/ITP1_9_C.py
@@ -1,7 +1,30 @@
 n   = int(input())
-Taro    = [0]*n
-Hanako  = [0]*n
+Taro_point  = 0
+Hanako_point    = 0
+
 for i in range(n):
-    Taro[i],Hanako[i]   = input().split()
-for i in range(n):
+    even_flag = 1
+    taro,hanako   = input().split()
+    taro    = taro.lower()
+    hanako  = hanako.lower()
+    for j in range(min(len(taro),len(hanako))):
+        if taro[j] > hanako[j]:
+            Taro_point += 3
+            even_flag = 0
+            break
+        elif hanako[j] > taro[j]:
+            Hanako_point += 3
+            even_flag = 0
+            break
+        else:
+            pass
     
+    if even_flag:
+        if len(taro) > len(hanako):
+            Taro_point += 3
+        elif len(hanako) > len(taro):
+            Hanako_point += 3
+        else:
+            Taro_point += 1
+            Hanako_point += 1
+print(Taro_point, Hanako_point)
---
+++ AtCoder/AtCoder_Beginner_Contest_116.py
@@ -0,0 +1,7 @@
+def C():
+    N   = int(input())
+    h_set   = [int(i) for i in input().split()]
+    n   = 0
+    while 1:
+        for h in h_set:
+            
